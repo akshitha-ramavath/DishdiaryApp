1a7e930ae6dc892bb30bf988d3e9cc5c66dbfd46
akshitha-ramavath <akshitharamavath686@gmail.com>
2025-03-02 15:09:40 +0530

    updated folder and files

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,19 @@ app.use(cors());
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
+mongoose.set('strictQuery', false); // Prepare for Mongoose 7 changes
+
 mongoose.connect(
-  "mongodb+srv://demomongo123:demomongo123@cluster0.bwegp.mongodb.net/recipetest?retryWrites=true&w=majority&appName=Cluster0",
+  "mongodb+srv://AkshithaRamavath:DD12345@dddb.s08bn.mongodb.net/",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(
-    console.log("DB connected...")
-  )
+  }).then(() => {
+    console.log("DB connected...");
+  }).catch(err => {
+    console.error("DB connection error:", err);
+  });
 
-app.listen(3001, () => console.log("Server started"));
+
+
+app.listen(5000, () => console.log("Server started on port 5000"));
